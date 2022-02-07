8711694d4c976ec23222f9562c2f20d68fa81bfa
Nirweint <nirweint_1@mail.ru>
2022-02-07 14:35:18 +0300

    fix contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Title} from "../../common/components/Title/Title";
 import Fade from 'react-reveal/Fade';
 import emailjs from '@emailjs/browser';
@@ -6,17 +6,24 @@ import s from './Contact.module.scss';
 
 export const Contact = () => {
 
+	const [email, setEmail] = useState('')
+
+	const handleEmailChange = (e) => {
+		setEmail(e.currentTarget.value)
+	}
 
 	const handleSendEmail = (e) => {
 		e.preventDefault();
-
-		emailjs.sendForm('service_3ainx4a', 'template_r9qonss', e.target, 'user_6cI0mlz5II9vPXIxiqYto')
-			.then((result) => {
-				console.log(result.text);
-			}, (error) => {
-				console.log(error.text);
-			});
-		e.target.reset();
+		if (/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)) {
+			emailjs.sendForm('service_3ainx4a', 'template_r9qonss', e.target, 'user_6cI0mlz5II9vPXIxiqYto')
+				.then((result) => {
+					console.log(result.text);
+				}, (error) => {
+					console.log(error.text);
+				});
+			alert("Message is sent")
+			e.target.reset();
+		}
 	}
 
 	return (
@@ -28,7 +35,7 @@ export const Contact = () => {
 						<input className={s.input} placeholder={'Name'} type={'text'}
 							   name={'name'}/>
 						<input className={s.input} placeholder={'Email'} type={'email'}
-							   name={'email'}/>
+							   name={'email'} onChange={handleEmailChange}/>
 						<textarea className={s.textarea} placeholder={'Message'}
 								  name={'message'}/>
 						<input className={s.btnInput} type='submit' value="Send"/>
